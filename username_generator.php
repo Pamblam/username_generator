54a11570859362b1b3b8a054695dad5e298bb8d2
Robert Parham <adelphia@protonmail.ch>
2016-09-12 20:30:57 -0400

    performance improvements

--- username_generator.php
+++ username_generator.php
@@ -3,12 +3,13 @@
 // if a pdo connection, table and column name are passed it will 
 // make sure the username isn't already used.
 function username_generator($pdo=null, $table=null, $column=null){
-	$nouns = explode("\n",file_get_contents("nouns.txt"));
-	$adjs = explode("\n",file_get_contents("adjs.txt"));
-	$username = ucfirst(strtolower($adjs[array_rand($adjs)])).
-		ucfirst(strtolower($nouns[array_rand($nouns)]));
+	$here = realpath(dirname(__FILE__));
+	$adjs = file("$here/adjs.txt");
+	$nouns = file("$here/nouns.txt");
+	$username = trim(ucfirst(strtolower($adjs[array_rand($adjs)]))).
+		trim(ucfirst(strtolower($nouns[array_rand($nouns)])));
 	if(empty($pdo)) return $username;
-	$q = $pdo->query("select count(1) from `$table` where `$column` = '$username'");
-	return !!count($q->fetch(2)) ? username_generator($pdo, $table, $column) : $username;
+	$q = $pdo->query("select count(1) as cnt from `$table` where `$column` = '$username'");
+	return !!$q->fetch(2)['cnt'] ? username_generator($pdo, $table, $column) : $username;
 }
 
